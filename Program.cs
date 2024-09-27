1aa217f8354d43accc33238011acfec2d0dfcf34
A7me2d <152171738+A7me2d@users.noreply.github.com>
2024-09-28 02:39:02 +0300

    full video

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Company.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
 namespace Company.Web
 {
     public class Program
@@ -8,6 +12,11 @@ namespace Company.Web
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
